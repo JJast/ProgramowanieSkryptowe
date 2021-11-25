e3def29d75b9688002df0a92e77c979e72ac8982
JJast <72971084+JJast@users.noreply.github.com>
2021-11-26 00:25:05 +0100

    Add files via upload

--- L6Z2/jednolinijkowiec2.py
+++ L6Z2/jednolinijkowiec2.py
@@ -1,14 +1,15 @@
-'''
+"""
 #!/usr/bin/python3
 
 import itertools
 
 listOfNumbers = list(itertools.chain.from_iterable(list(map(lambda x: open(x).read().split(), input().split()))))
 test = list(filter(lambda num: int(num) % 2 == 0, listOfNumbers))
-print(test)
+print(len(test))
 
 
-print(list(filter(lambda num: int(num) % 2 == 0, itertools.chain.from_iterable(list(map(lambda x: open(x).read().split(), input().split())))))) # WYSWIETLA LISTE PARZYSTYCH
-'''
+print(len(list(filter(lambda num: int(num) % 2 == 0, itertools.chain.from_iterable(list(map(lambda x: open(x).read().split(), input().split()))))))) # WYSWIETLA LISTE PARZYSTYCH
+"""
 
-import sys;import itertools;print(len(list(filter(lambda num: int(num) % 2 == 0, itertools.chain.from_iterable(list(map(lambda x: open(x).read().split(), sys.argv[1:])))))))
+
+import sys;import itertools; print(len(list(filter(lambda num: int(num) % 2 == 0, list(itertools.chain.from_iterable(list(map(lambda x: open(x).read().split(), sys.argv[1:]))))))))
