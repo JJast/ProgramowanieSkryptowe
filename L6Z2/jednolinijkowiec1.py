e04a291cfd26639ad3e51081d705f56e298f7ba3
JJast <72971084+JJast@users.noreply.github.com>
2021-11-26 09:43:53 +0100

    Update jednolinijkowiec1.py

--- L6Z2/jednolinijkowiec1.py
+++ L6Z2/jednolinijkowiec1.py
@@ -3,7 +3,6 @@ import sys
 from collections import Counter
 
 wordsLength = list(map(lambda x: len(x), sys.stdin.read().split()))
-#wordsCount = map(lambda x: (x, wordsLength.count(x)), wordsLength)
 wordsCount = Counter(wordsLength)
 print(dict(wordsCount))
 '''
